3086ea652f5accdf89efa089b6b615f58a27c5b3
BuildTools <unconfigured@null.spigotmc.org>
2016-06-06 19:05:55 -0500

    My Name Is Jeff

--- src/team/tekkitandtiger/minigame/Minigame.java
+++ src/team/tekkitandtiger/minigame/Minigame.java
@@ -7,6 +7,7 @@ public class Minigame extends JavaPlugin {
 	
 	/**
 	 * TODO: EVERYTHING
+	 * My Name Is Jeff/
 	 */
 	public void onEnable() {
 		Bukkit.getServer().getLogger().info("[Minigame] Enabled!");
